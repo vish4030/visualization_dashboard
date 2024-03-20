ead5ea2b39daaf56da57a78bd8b7220fa5c50dd8
vish4030 <vishwajeetkumarraj06@gmail.com>
2024-03-20 14:19:24 +0530

    chart filter with year

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,45 @@
+import { useState, useEffect } from "react"
+import {Chart as ChartJs} from 'chart.js/auto';
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PiChat";
+import PolarArChart from "./components/PolarAreaChart";
+import RadarChart from "./components/RadarChat";
+import ScatterChart from "./components/ScatterChart";
+import DoughnutChart from "./components/DoughnutChart";
+import BubbleChart from "./components/BubbleChart";
+
 
 
 function App() {
  
+  const [data, setData] = useState([]);
 
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+  const fetchData = async()=>{
+    const res = await fetch('http://localhost:3000/data');
+      const json = await res.json();
+      setData(json);
+  }
+  
+  useEffect(()=>{
+    fetchData(); 
+  },[])
+
+ if(data.length<1)
+  return <h1>Loading...</h1>
+ return(
+    <div className="container">
+      <div className="row">
+        <div className="col-6"><BarChart data={data}/></div>
+        <div className="col-6"><LineChart data={data}/></div>
+        <div className="col-6"><ScatterChart data={data}/></div>
+        <div className="col-6"><BubbleChart data={data}/></div>
+        <div className="col-6"><RadarChart data={data}/></div>
+        <div className="col-6"><PolarArChart data={data}/></div>
+        <div className="col-6"><PieChart data={data}/></div>
+        <div className="col-6"><DoughnutChart data={data}/></div>
+      </div>  
+    </div>) 
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 import dataRouter from "./routes/data.js";
 
 export const app = express();
 
+app.use(cors());
 app.use(dataRouter);
 
